4caba20c68d6dde190099836ce6516f3905245ec
Voa_Disco <borelabryan947@gmail.com>
2024-10-10 09:06:11 -0300

    gg

--- Voa_Disco/GamePage.xaml.cs
+++ Voa_Disco/GamePage.xaml.cs
@@ -1,10 +1,37 @@
 namespace Voa_Disco;
 public partial class GamePage : ContentPage
 {
-	
+const int Gravidade=5;
+const int TempoEntreFrames=40;
+bool EstarMorto=false;
 
-	public GamePage()
+    public GamePage()
 	{
 		InitializeComponent();
 	}
+	void AplicaGravidade()
+	{
+		navelol.TranslationY +=Gravidade;
+	}
+	
+
+     async Task Desenhar()
+	 {
+       while (!EstarMorto)
+       {
+		AplicaGravidade();
+		await Task.Delay(TempoEntreFrames);
+	   }
+	 }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenhar();
+    }
+
+
+
+
+
+
 }
