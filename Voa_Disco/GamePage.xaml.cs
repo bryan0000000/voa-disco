d199e134331c58c11392b3c265e788ae076f80ac
Voa_Disco <borelabryan947@gmail.com>
2024-10-11 11:10:22 -0300

    ajustar

--- Voa_Disco/GamePage.xaml.cs
+++ Voa_Disco/GamePage.xaml.cs
@@ -2,8 +2,17 @@ namespace Voa_Disco;
 public partial class GamePage : ContentPage
 {
 const int Gravidade=5;
-const int TempoEntreFrames=40;
-bool EstarMorto=false;
+const int TempoEntreFrames=20;
+bool EstarMorto=true;
+double LarguraDaJanela=0;
+double AlturaDaJanela=0;
+int Velocidade=7;
+
+void Inicializar()
+{
+	EstarMorto=false;
+	navelol.TranslationY=0;
+}
 
     public GamePage()
 	{
@@ -21,13 +30,39 @@ bool EstarMorto=false;
        {
 		AplicaGravidade();
 		await Task.Delay(TempoEntreFrames);
+		MovimentacaoDosCanos();
 	   }
 	 }
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-		Desenhar();
-    }
+    
+
+protected override void OnSizeAllocated ( double w, double h )
+{
+  LarguraDaJanela=w;
+  AlturaDaJanela=h;
+}
+void MovimentacaoDosCanos()
+{
+ cano1.TranslationX -= Velocidade;
+ cano2.TranslationX -=Velocidade;
+
+ if (cano1.TranslationX<-LarguraDaJanela)
+ {
+	cano1.TranslationX=0;
+    cano2.TranslationX=0;
+ }
+
+}
+
+void morreu( object s,TappedEventArgs a)
+{
+
+	framemorreu.IsVisible =false;
+	Inicializar();
+	Desenhar();
+}
+
+
+
 
 
 
